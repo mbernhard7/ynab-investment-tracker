e69cd76f001247ece7eb0e88b8c2a7d4f59fe5bc
Miles Bernhard <mbernhard22@students.claremontmckenna.edu>
2025-01-27 23:14:20 -0500

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import {Account, API, BulkTransactions, SaveTransaction, TransactionsResponseData} from "ynab";
-import yahooFinance from 'yahoo-finance2';
-import ClearedEnum = SaveTransaction.ClearedEnum;
-import FlagColorEnum = SaveTransaction.FlagColorEnum;
-import { QuoteResponseMap} from "yahoo-finance2/dist/esm/src/modules/quote";
+import {Account, API, BulkTransactions, TransactionsResponseData} from "ynab";
+import yahooFinance from "yahoo-finance2";
+import {QuoteResponseArray} from "yahoo-finance2/dist/esm/src/modules/quote";
+import {TransactionFlagColor} from "ynab/dist/models/TransactionFlagColor";
+import {TransactionClearedStatus} from "ynab/dist/models/TransactionClearedStatus";
 
 type THoldings = Map<string, { count: number, value: number }>;
 
@@ -24,8 +24,60 @@ export const run = async () => {
         const investmentAccounts = accounts.filter((account) => account?.note?.includes("INVESTMENT_ACCOUNT"));
         console.log(`[YNIT] Found ${investmentAccounts.length} investment accounts.`);
 
-        await Promise.all(investmentAccounts.map(account => processAccount(ynabAPI, budgetID, account)));
+        const accountsWithHoldings = await Promise.all(investmentAccounts.map(async account => {
+                const holdings = await processAccount(ynabAPI, budgetID, account)
+                return {account, holdings}
+            }
+        ));
+
+        const symbolsToFetch = []
+        accountsWithHoldings.forEach(({holdings}) => {
+            [...holdings.keys()].forEach(k => {
+                if (k !== "CASH" && symbolsToFetch.find(j => j === k) === undefined) {
+                    symbolsToFetch.push(k)
+                }
+            })
+        })
+
+        if (symbolsToFetch.length) {
+            console.log(`[YNIT] Fetching quotes for: ${symbolsToFetch}`);
+            const quotes: QuoteResponseArray = await yahooFinance.quote(symbolsToFetch, {fields: ["regularMarketPrice"]});
+            console.log(`[YNIT] Fetched quotes.`);
+            console.log(`[YNIT] Current prices:`);
+            quotes.forEach((q) => {
+                console.log(`       [${q.symbol}] Price: ${q.regularMarketPrice}`);
+            });
+
+            const bulkTransactions = await Promise.all(accountsWithHoldings.map(async ({
+                                                                                           account,
+                                                                                           holdings
+                                                                                       }) => await buildUpdates(account, holdings, quotes)))
+
+            const mergedTransactions = bulkTransactions.reduce(({transactions: at}, {transactions: vt}) => ({transactions: [...at, ...vt]}), {transactions: []})
+
+            if (mergedTransactions.transactions.length) {
+                console.log(`[YNIT] Posting transactions to YNAB...`);
+                await ynabAPI.transactions.createTransactions(budgetID, mergedTransactions);
+                console.log(`[YNIT] Posted transactions.`);
+            }
+        } else {
+            console.log(`[YNIT] No symbols to fetch.`);
+        }
     }
+}
+
+const buildUpdates = async (account: Account, accountHoldings: THoldings, quotes: QuoteResponseArray) => {
+    const bulkTransactions = buildUpdateTransactionsFromQuotesAndHoldings(account, quotes, accountHoldings)
+
+    if (bulkTransactions.transactions.length === 0) {
+        console.log(`[YNIT] No updates needed for ${account.name}.`);
+    } else {
+        console.log(`[YNIT] Updates for ${account.name}:`);
+        bulkTransactions.transactions.forEach((tx) => {
+            console.log(`       [${tx.memo}] $${tx.amount / 1000}`);
+        });
+    }
+    return bulkTransactions
 }
 
 const processAccount = async (ynabAPI: API, budgetID: string, account: Account) => {
@@ -37,40 +89,15 @@ const processAccount = async (ynabAPI: API, budgetID: string, account: Account) 
 
     const accountHoldings = buildAccountHoldingsFromTransactions(data)
 
-    console.log(`[YNIT] Current holdings:`);
+    console.log(`[YNIT] Current holdings for ${account.name}:`);
     accountHoldings.forEach(({count, value}, ticker) => {
         console.log(`       [${ticker}] Shares: ${count} Value: $${value}`);
     });
-
-
-    if (Array.from(accountHoldings.keys()).length) {
-        console.log(`[YNIT] Fetching quotes...`);
-        const quotes: QuoteResponseMap = await yahooFinance.quote(Array.from(accountHoldings.keys()).filter(k => k !== "CASH"), { fields: ["regularMarketPrice"], return: "map" });
-        console.log(`[YNIT] Fetched quotes.`);
-        console.log(`[YNIT] Current prices:`);
-        [...quotes.keys()].forEach((key) => {
-            console.log(`       [${key}] Price: ${quotes.get(key).regularMarketPrice}`);
-        });
-
-        const bulkTransactions = buildUpdateTransactionsFromQuotesAndHoldings(account, quotes, accountHoldings)
-
-        if (bulkTransactions.transactions.length ===0) {
-            console.log(`[YNIT] No updates needed.`);
-        } else {
-            console.log(`[YNIT] Updates:`);
-            bulkTransactions.transactions.forEach((tx) => {
-                console.log(`       [${tx.memo}] $${tx.amount/1000}`);
-            });
-
-            console.log(`[YNIT] Posting transactions to YNAB...`);
-            await ynabAPI.transactions.bulkCreateTransactions(budgetID, bulkTransactions);
-            console.log(`[YNIT] Posted transactions.`);
-        }
-    }
+    return accountHoldings
 }
 
-const buildAccountHoldingsFromTransactions = ( data: TransactionsResponseData): THoldings => {
-    const accountHoldings: THoldings  = new Map();
+const buildAccountHoldingsFromTransactions = (data: TransactionsResponseData): THoldings => {
+    const accountHoldings: THoldings = new Map();
 
     const transactions = data.transactions.map(t => {
         return {...t, date: Date.parse(t.date)};
@@ -80,9 +107,9 @@ const buildAccountHoldingsFromTransactions = ( data: TransactionsResponseData): 
 
     transactions.forEach(t => {
         if (t?.memo && t?.memo?.startsWith("$")) {
-            const [ticker, action] = t.memo.replace("$","").split("|")
+            const [ticker, action] = t.memo.replace("$", "").split("|")
             const holding = accountHoldings.get(ticker) || {count: 0, value: 0};
-            holding.value = holding.value + (t.amount/1000);
+            holding.value = holding.value + (t.amount / 1000);
             if (action.startsWith("BUY")) {
                 holding.count = holding.count + +action.split(" ")[1];
             }
@@ -96,7 +123,7 @@ const buildAccountHoldingsFromTransactions = ( data: TransactionsResponseData): 
             }
         } else {
             const holding = accountHoldings.get("CASH") || {count: 0, value: 0};
-            holding.value = holding.value + (t.amount/1000);
+            holding.value = holding.value + (t.amount / 1000);
             accountHoldings.set("CASH", holding);
         }
     });
@@ -105,29 +132,31 @@ const buildAccountHoldingsFromTransactions = ( data: TransactionsResponseData): 
     return accountHoldings;
 }
 
-const buildUpdateTransactionsFromQuotesAndHoldings = (account: Account, quotes: QuoteResponseMap, holdings: THoldings): BulkTransactions => {
+const buildUpdateTransactionsFromQuotesAndHoldings = (account: Account, quotes: QuoteResponseArray, holdings: THoldings): BulkTransactions => {
     const bulkTransactions: BulkTransactions = {transactions: []};
     const currentDate = new Date();
     const offset = currentDate.getTimezoneOffset();
 
     holdings.forEach(({count, value}, ticker) => {
-        const quote = quotes.get(ticker);
+        const quote = quotes.find(k => k.symbol === ticker);
         if (!quote?.regularMarketPrice) {
-            console.log(`[WARNING] Failed to get quote for: ${ticker}`);
+            if (ticker !== "CASH") {
+                console.log(`[WARNING] Failed to get quote for: ${ticker}`);
+            }
         } else {
             const difference = (quote.regularMarketPrice * count) - value;
 
-            if (Math.round(difference*1000) !== 0) {
+            if (Math.round(difference * 1000) !== 0) {
 
                 bulkTransactions.transactions.push({
                     "account_id": account.id,
-                    "date": new Date(currentDate.getTime() - (offset*60*1000)).toISOString().split('T')[0],
-                    "amount": Math.round(difference*1000),
+                    "date": new Date(currentDate.getTime() - (offset * 60 * 1000)).toISOString().split("T")[0],
+                    "amount": Math.round(difference * 1000),
                     "payee_name": `Investment Value Update`,
                     "memo": `$${ticker}|VALUE_UPDATE`,
-                    "cleared": ClearedEnum.Cleared,
+                    "cleared": TransactionClearedStatus.Cleared,
                     "approved": true,
-                    "flag_color": FlagColorEnum.Blue
+                    "flag_color": TransactionFlagColor.Blue
                 });
             }
         }
